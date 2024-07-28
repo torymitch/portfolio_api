0f589daf4d4dfb1962af262da5b7e56f75b7b4c2
Tory Mitchell <torymitch@gmail.com>
2024-07-28 17:22:07 -0400

    JPA is created for getUsers

---
+++ src/main/java/com/api/service/dao/UserDao.java
@@ -0,0 +1,18 @@
+package com.api.service.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.api.model.User;
+import com.api.repository.UserRepository;
+
+@Component("userDao")
+public class UserDao {
+	
+	@Autowired 
+	private UserRepository userRepository;
+	
+	public Iterable<User> getUsers() {
+		return userRepository.findAll();
+	}
+}
